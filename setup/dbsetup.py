e3dd6814140251e53fda84ad1ee591009c61079c
Matheus Cruz <mlcruz@inf.ufrgs.br>
2021-03-28 00:05:29 -0300

    test data

--- setup/dbsetup.py
+++ setup/dbsetup.py
@@ -1,32 +1,40 @@
 import requests
 import json
 
-USER_ENDPOINT = "http://localhost:8000/user/"
-DOCTOR_ENDPOINT = "http://localhost:8000/doctor/"
-CONSULTATION_ENDPOINT = "http://localhost:8000/appointment/"
+# IP PROD = 54.232.147.115 
+HOST_IP = "127.0.0.1"
+
+USER_ENDPOINT = f"http://{HOST_IP}/user/"
+APPOINTMENT_ENDPOINT = f"http://{HOST_IP}/appointment/"
 
 
-def add_user(first_name, last_name, login, password):
+def add_user(name, login, password, birth_date, gender, user_type):
     result = requests.post(
         USER_ENDPOINT,
         json={
-            "first_name": first_name,
-            "last_name": last_name,
+            "name": name,
             "login": login,
             "password": password,
+            "birth_date": birth_date,
+            "gender": gender,
+            "user_type": user_type,
         },
     )
+
+    print(result.content)
     if not result.ok:
         assert result.ok
 
 
-def add_doctor(first_name, last_name, specialty, address):
+def add_appointment(patience_id, doctor_id, start, end, address, extra_data=None):
     result = requests.post(
-        DOCTOR_ENDPOINT,
+        APPOINTMENT_ENDPOINT,
         json={
-            "first_name": first_name,
-            "last_name": last_name,
-            "specialty": specialty,
+            "patience": patience_id,
+            "doctor": doctor_id,
+            "start": start,
+            "end": end,
+            "extra_data": json.dumps(extra_data),
             "address": address,
         },
     )
@@ -34,103 +42,37 @@ def add_doctor(first_name, last_name, specialty, address):
         assert result.ok
 
 
-def add_appointment(user_id, doctor_id, start, end, extra_data=None):
-    result = requests.post(
-        CONSULTATION_ENDPOINT,
-        json={
-            "user_id": user_id,
-            "doctor_id": doctor_id,
-            "start": start,
-            "end": end,
-            "extra_data": json.dumps(extra_data),
-        },
-    )
-    if not result.ok:
-        assert result.ok
+# name, login, password, birth_date, gender, user_type
+add_user("joaozinho", "joaozinho", "teste", "1990-03-25", "MALE", "PATIENCE")
+add_user("mariazinha", "mariazinha", "teste", "1990-03-25", "FEMALE", "PATIENCE")
 
 
-def model_exists(endpoint, id):
-    result = requests.get(f"{endpoint}{id}")
-    return result.ok
-
-
-def get_model_object(endpoint, id):
-    result = requests.get(f"{endpoint}{id}")
-    return json.loads(result.text)
-
-
-def add_if_undefined(endpoint, id, *args):
-    if not model_exists(endpoint, id):
-        if endpoint == USER_ENDPOINT:
-            add_user(*args)
-        elif endpoint == DOCTOR_ENDPOINT:
-            add_doctor(*args)
-        elif endpoint == CONSULTATION_ENDPOINT:
-            add_appointment(*args)
-        print(get_model_object(endpoint, id))
-    else:
-        print(get_model_object(endpoint, id))
-
-
-add_if_undefined(USER_ENDPOINT, 1, "joaozinho", "teste", "teste", "123456")
-add_if_undefined(DOCTOR_ENDPOINT, 1, "maria", "teste", "cardiologista", "Rua teste 1")
-add_if_undefined(
-    DOCTOR_ENDPOINT, 2, "jorge", "testudo", "dermatologista", "Rua teste 1"
-)
-add_if_undefined(DOCTOR_ENDPOINT, 3, "miguel", "barbiz", "ortopedista", "Rua teste 1")
-add_if_undefined(DOCTOR_ENDPOINT, 4, "carla", "bar", "neurologista", "Rua teste 1")
-add_if_undefined(DOCTOR_ENDPOINT, 5, "luciana", "foo", "oftalmologista", "Rua teste 1")
-add_if_undefined(
-    DOCTOR_ENDPOINT, 6, "joaquim", "foobar", "clínico geral", "Rua teste 1"
-)
+# Doctors
+add_user("jorge", "jorge", "teste", "1980-03-23", "MALE", "DOCTOR")
+add_user("joaquinha", "joaquina", "teste", "1980-01-25", "FEMALE", "DOCTOR")
+add_user("miguel", "miguel", "teste", "1970-07-15", "MALE", "DOCTOR")
+add_user("carla", "carla", "teste", "1970-07-15", "FEMALE", "DOCTOR")
 
 # id,user,doctor,datetime
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
-    1,
-    1,
-    1,
-    "2021-03-25T12:00:00-03:00",
-    "2021-03-25T12:45:00-03:00",
-)
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
-    1,
-    1,
-    2,
-    "2021-03-26T12:00:00-03:00",
-    "2021-03-26T12:45:00-03:00",
-)
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
-    1,
-    1,
-    3,
-    "2021-03-27T12:00:00-03:00",
-    "2021-03-27T12:45:00-03:00",
-)
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
-    1,
-    1,
-    4,
-    "2021-03-28T12:00:00-03:00",
-    "2021-03-28T12:45:00-03:00",
-)
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
-    1,
-    1,
-    5,
-    "2021-03-29T12:00:00-03:00",
-    "2021-03-29T12:45:00-03:00",
+# patience_id, doctor_id, start, end, address,extra_data=None
+add_appointment(
+    1, 3, "2021-03-25T12:00:00-03:00", "2021-03-25T12:45:00-03:00", "Rua Teste 1."
 )
 
-add_if_undefined(
-    CONSULTATION_ENDPOINT,
+
+add_appointment(
+    2, 3, "2021-03-26T12:00:00-03:00", "2021-03-26T12:45:00-03:00", "Rua Teste 1."
+)
+
+add_appointment(
+    1, 4, "2021-03-27T12:00:00-03:00", "2021-03-27T12:45:00-03:00", "Rua Teste 2."
+)
+
+
+add_appointment(
     1,
-    1,
-    6,
+    5,
     "2021-03-30T12:00:00-03:00",
     "2021-03-30T12:45:00-03:00",
+    "Rua Teste 3.",
 )
