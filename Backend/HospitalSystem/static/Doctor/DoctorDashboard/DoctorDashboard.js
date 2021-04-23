ee17a636263b8cb75815872e190d1bd88508f021
Matheus Cruz <mlcruz@inf.ufrgs.br>
2021-04-22 21:42:13 -0300

    doctor frontend api calls

--- Backend/HospitalSystem/static/Doctor/DoctorDashboard/DoctorDashboard.js
+++ Backend/HospitalSystem/static/Doctor/DoctorDashboard/DoctorDashboard.js
@@ -12,21 +12,12 @@ function init() {
         cancelAppointment();
     });
 
-    // Mock for creating itens
-    testAppointment1 = new Appointment('5011', 'Amanda Pires', '12:10-12:30')
-    testAppointment2 = new Appointment('5012', 'Carlos Hickmann', '14:30-15:30')
-    testAppointment3 = new Appointment('5013', 'João Nascimento', '11:30-11:45')
-    testAppointment4 = new Appointment('5014', 'Beatriz Ribeiro', '18:30-18:50')
-    testAppointment5 = new Appointment('5015', 'Joana Telles', '19:00-19:30')
-    testAppointment6 = new Appointment('5016', 'Marta Nascimenton', '19:30-20:00')
-    testAppointment7 = new Appointment('5017', 'Orlando Wender', '20:30-21:00')
-    createAppointmentItem(testAppointment1)
-    createAppointmentItem(testAppointment2)
-    createAppointmentItem(testAppointment3)
-    createAppointmentItem(testAppointment4)
-    createAppointmentItem(testAppointment5)
-    createAppointmentItem(testAppointment6)
-    createAppointmentItem(testAppointment7)
+    getAppointmentsRequest().then((s) => {
+        s.forEach(i => {
+          createAppointmentItem(i);
+        })
+        console.log(s);
+      });
 }
 
 //*************************************************
@@ -96,6 +87,57 @@ function cancelAppointmentRequest() {
     // TODO: Implement request to cancel an appointment
 }
 
-function getAppointmentsRequest() {
-    // TODO: Implement request to get patient appointments
+async function getAppointmentsRequest() {
+    try {
+      const appointments = (
+        await ApiClient.get(
+          `appointment?user_type=DOCTOR&appointment_status=DONE`
+        )
+      ).map(
+        (appointment) =>
+          new Appointment(appointment.id, appointment.doctor.name, appointment.start)
+      );
+      console.log(appointments);
+  
+      return appointments;
+    } catch(err) {
+      console.error(err);
+
+      //offline fallback
+      testAppointment1 = new Appointment("5011", "Amanda Pires", "12:10-12:30");
+      testAppointment2 = new Appointment(
+        "5012",
+        "Carlos Hickmann",
+        "14:30-15:30"
+      );
+      testAppointment3 = new Appointment(
+        "5013",
+        "João Nascimento",
+        "11:30-11:45"
+      );
+      testAppointment4 = new Appointment(
+        "5014",
+        "Beatriz Ribeiro",
+        "18:30-18:50"
+      );
+  
+      testAppointment5 = new Appointment("5015", "Joana Telles", "19:00-19:30");
+      testAppointment6 = new Appointment(
+        "5016",
+        "Marta Nascimenton",
+        "19:30-20:00"
+      );
+  
+      testAppointment7 = new Appointment("5017", "Orlando Wender", "20:30-21:00");
+  
+      return [
+        testAppointment1,
+        testAppointment2,
+        testAppointment3,
+        testAppointment4,
+        testAppointment5,
+        testAppointment6,
+        testAppointment7,
+      ];
+    }
 }
