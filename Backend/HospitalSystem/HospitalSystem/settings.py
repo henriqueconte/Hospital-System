7dfabf60834a520ed55a26c3af071fedf037c1eb
Pedro <ph.pfiorentin@gmail.com>
2021-04-23 14:58:17 -0300

    changed the urls

--- Backend/HospitalSystem/HospitalSystem/settings.py
+++ Backend/HospitalSystem/HospitalSystem/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "uAKIATBKJ7TE66K5U3MO"
+SECRET_KEY = "kd#jafh_%cpa2iud7ll%$baz^@qh8)l@&k!y^lm)aot52syk+#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
